799fa986ac49785c9629a345e0c8dd1f833eee25
Denis Sorokin <denys-s@yit.co.il>
2017-06-28 16:09:42 +0300

    promises not finished

--- src/promises.js
+++ src/promises.js
@@ -1,11 +1,23 @@
-function applyForVisa(documents, resolve) {
+function applyForVisa(documents) {
     console.log('Processing ...')
-    setTimeout(() => {
-        let visa = {}
-        resolve(visa)
-    }, 2000)
+    return new Promise(function (resolve, reject) {
+        setTimeout(() => {
+            Math.random() > .5 ? resolve({}) : reject('Not enough documents')
+        }, 2000)
+    })
 }
 
-applyForVisa({}, function(visa){
-    console.info('Visa is ready')
-})
+function bookHotel() {
+    console.log('Booking hotel...')
+}
+function buyTickets() {
+    console.log('Buying tickets')
+}
+
+applyForVisa({})
+    .then(visa => console.info('Visa is ready'))
+    .then(bookHotel)
+    .then(buyTickets)
+    .catch(error => console.error(error))
+
+
